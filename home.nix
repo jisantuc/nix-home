6a06af7b30da63292626bb6869ee2f1ba1967b73
James Santucci <james.santucci@gmail.com>
2022-10-20 22:15:10 -0600

    Configure neovim with metals and other goodies (#18)
    * wip
    
    * Configure metals
    
    * Set home directory more intelligently
    
    * Be consistent with top-level programs keys
    
    * Add neogit plugin
    
    * Add neovim-tree-lua
    
    * Patch to disable failing hm dependency
    
    * Add two nice fonts, just in case
    
    * pin java version for sbt
    
    * wip
    
    * Remove unused package
    
    * Set some nice mappings for diagnostics
    
    * Use env just for finding home dir
    
    feels gross but maybe it's fine
    
    * Fix whitespace
    
    * Improve neogit / tree view launch
    
    * Remove old copy of viml config
    
    * Remove old diagnostic action, add codelens action
    
    * Restore man
    
    Co-authored-by: James Santucci <jamessantucii@c02fk0dxpt01-ff.lan>

--- home.nix
+++ home.nix
@@ -9,8 +9,11 @@ let
     pkgs.bash-completion
   ];
 
-  scalaDeps = [
+  jvmDeps = [
     pkgs.ammonite
+    pkgs.coursier
+    (pkgs.sbt.override { jre = pkgs.openjdk11; })
+    pkgs.jdt-language-server
   ];
 
   systemUtilities = [
@@ -29,12 +32,17 @@ let
     pkgs.rnix-lsp
   ];
 
+  patchedFonts = [
+    # need the parens because otherwise the space makes it look like list entries
+    (pkgs.nerdfonts.override { fonts = [ "Hasklig" "SourceCodePro" ]; })
+  ];
+
 in
-{
+rec {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "james";
-  home.homeDirectory = "/Users/james";
+  home.homeDirectory = builtins.getEnv "HOME";
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
@@ -46,14 +54,47 @@ in
   # changes in each release.
   home.stateVersion = "22.05";
 
+
+  xdg.configFile.nvim = {
+    source = ./neovim;
+    recursive = true;
+  };
+
+
   programs = {
+
     # Let Home Manager install and manage itself.
     home-manager.enable = true;
 
     # Enable and configure tmux
-    tmux.enable = true;
-    tmux.extraConfig = builtins.readFile ./dotfiles/tmux.conf;
+    tmux = {
+      enable = true;
+      extraConfig = builtins.readFile ./dotfiles/tmux.conf;
+    };
+
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+
+      plugins =
+        with pkgs.vimPlugins;
+        [
+          fzf-lua
+          neogit
+          nvim-dap
+          nvim-jdtls
+          nvim-metals
+          nvim-tree-lua
+          nvim-web-devicons
+          plenary-nvim
+          vim-colors-solarized
+          vim-nix
+        ];
+    };
   };
 
-  home.packages = shellUtilities ++ systemUtilities ++ scalaDeps ++ ourobouros;
+  fonts.fontconfig.enable = true;
+
+  home.packages = shellUtilities ++ systemUtilities ++ jvmDeps ++ ourobouros ++ patchedFonts;
 }
---
+++ neovim/init.lua
@@ -0,0 +1,79 @@
+-- customize nvim-tree rendering
+require("nvim-tree").setup({
+  renderer = {
+    group_empty = true,
+    highlight_git = true
+  },
+  view = {
+    adaptive_size = true
+  },
+  diagnostics = { enable = true },
+  filters = { dotfiles = false }
+})
+
+-- activate and configure metals
+local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "scala", "sbt", "java" },
+  callback = function()
+    require("metals").initialize_or_attach({})
+  end,
+  group = nvim_metals_group,
+})
+
+metals_config = require("metals").bare_config()
+metals_config.settings = {
+       showImplicitArguments = true,
+       excludedPackages = {
+        "akka.actor.typed.javadsl",
+        "com.github.swagger.akka.javadsl"
+       }
+}
+metals_config.init_options.statusBarProvider = "on"
+
+-- configure lsp keybindings
+vim.api.nvim_set_keymap(
+  "n", -- mode
+  "gd", -- key chord
+  "<cmd>lua vim.lsp.buf.definition()<CR>", -- command to execute
+  { noremap = true } -- some options, I don't know what I can do here
+)
+
+vim.api.nvim_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "gds", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "gws", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>cl", "<cmd>lua vim.lsp.codelens.run()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>aa", "<cmd>lua vim.diagnostic.setqflist()<CR>", { noremap = true }) -- all workspace diagnostics
+vim.api.nvim_set_keymap("n", "<leader>ae", "<cmd>lua vim.diagnostic.setqflist({severity = \"E\"})<CR>", { noremap = true }) -- all workspace errors
+vim.api.nvim_set_keymap("n", "<leader>aw", "<cmd>lua vim.diagnostic.setqflist({severity = \"W\"})<CR>", { noremap = true }) -- all workspace warnings
+vim.api.nvim_set_keymap("n", "<leader>d", "<cmd>lua vim.diagnostic.setloclist()<CR>", { noremap = true }) -- buffer diagnostics only
+
+-- make find much better
+vim.opt.path:remove "/usr/include"
+vim.opt.path:append "**"
+vim.opt.wildignorecase = true
+vim.opt.wildignore:append "**/node_modules/*"
+vim.opt.wildignore:append "**/target/*"
+vim.opt.wildignore:append "**/.bloop/*"
+vim.opt.wildignore:append "**/.bsp/*"
+vim.opt.wildignore:append "**/.metals/*"
+
+-- launch git conveniently
+vim.api.nvim_set_keymap("n", "<space>gs", "<cmd>:Neogit<CR>", { noremap = true })
+
+-- launch tree view conveniently
+vim.api.nvim_set_keymap("n", "<space>tt", "<cmd>:NvimTreeToggle<CR>", { noremap = true })
+
+require("nvim-web-devicons").setup {
+  color_icons = true;
+  default = true;
+}
+
+-- make colors look nice
+vim.opt.background = "light"
+vim.cmd("colorscheme solarized")
