19e47790530783c28730f36525348599b8d76e6e
James Santucci <james.santucci@flockfreight.com>
2022-12-06 15:41:30 -0700

    Add binding for project search

--- neovim/lua/fzf-config.lua
+++ neovim/lua/fzf-config.lua
@@ -10,4 +10,5 @@ vim.opt.wildignore:append "**/.metals/*"
 vim.api.nvim_set_keymap("n", "<space>ff", "<cmd>:FzfLua files<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<space>fb", "<cmd>:FzfLua buffers<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<space>fg", "<cmd>:FzfLua git_files<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<space>fs", "<cmd>:FzfLua grep_project<CR>", { noremap = true })
 
