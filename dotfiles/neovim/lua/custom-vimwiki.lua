ca264f333c992e4e2b615647011caee3ba7d9259
James Santucci <james.santucci@gmail.com>
2023-03-08 16:28:57 -0700

    add backlinks binding

--- dotfiles/neovim/lua/custom-vimwiki.lua
+++ dotfiles/neovim/lua/custom-vimwiki.lua
@@ -1,2 +1,3 @@
 -- add a binding for generating table of contents
 vim.api.nvim_set_keymap("n", "<leader>wc", "<cmd>VimwikiTOC<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>wb", "<cmd>VimwikiBacklinks<CR>", { noremap = true })
