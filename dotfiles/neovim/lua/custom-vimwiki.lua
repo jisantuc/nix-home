c1944f6864873a0c7fcf2cacce6a57aff8405b4e
James Santucci <james.santucci@gmail.com>
2023-07-07 10:54:37 -0600

    Add a binding to open local, relative vimwiki (#59)
    * Add a binding to open local, relative vimwiki
    
    * Upgrade install-nix-action

--- dotfiles/neovim/lua/custom-vimwiki.lua
+++ dotfiles/neovim/lua/custom-vimwiki.lua
@@ -1,4 +1,12 @@
--- add a binding for generating table of contents
+-- list things that link to the currently opened wiki file
 vim.api.nvim_set_keymap("n", "<leader>wlb", "<cmd>VimwikiBacklinks<CR>", { noremap = true })
+-- confirm all links in this wiki are reachable
 vim.api.nvim_set_keymap("n", "<leader>wlc", "<cmd>VimwikiCheckLinks<CR>", { noremap = true })
+-- generate a table of contents
 vim.api.nvim_set_keymap("n", "<leader>wc", "<cmd>VimwikiTOC<CR>", { noremap = true })
+-- open local wiki index file -- "wiki here"
+-- this is a workaround for lack of discovery in vimwiki and my personal desire not to write
+-- absolute paths into my registered wikis
+-- the opened wiki will be a "temporary wiki" (see `:h vimwiki-temporary-wiki`) but that's ok,
+-- everything will still work within the nvim session
+vim.api.nvim_set_keymap("n", "<leader>wh", "<cmd>:e .wiki/index.wiki<CR>", { noremap = true })
