11a967d8f983296be233e124cbf511d87f76ed02
James Santucci <james.santucci@gmail.com>
2023-08-04 14:16:28 -0600

    Add outputs and script updates for arm64 macos

--- flake.nix
+++ flake.nix
@@ -5,23 +5,18 @@
     nixpkgs.url = "nixpkgs/nixos-22.11";
 
     home-manager.url = "github:nix-community/home-manager";
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, flake-utils, ... }:
 
     let
       mkHome = { system }: import ./hm-lib.nix {
         inherit nixpkgs home-manager system;
       };
     in
-    {
-      homeConfigurations = {
-        linux = mkHome {
-          system = "x86_64-linux";
-        };
-        macOS = mkHome {
-          system = "x86_64-darwin";
-        };
-      };
-    };
+    flake-utils.lib.eachDefaultSystem (system: {
+      homeConfigurations = mkHome { inherit system; };
+    });
 }
