8c0b0cfd519cfdf25d6aaeff3e1a0f719c57f51a
James Santucci <james.santucci@gmail.com>
2023-08-04 14:23:33 -0600

    Add outputs and script updates for arm64 macos (#62)

--- hm-lib.nix
+++ hm-lib.nix
@@ -1,12 +1,7 @@
 { nixpkgs, home-manager, system, ... }:
-
-let
+home-manager.lib.homeManagerConfiguration rec {
   pkgs = import nixpkgs {
     inherit system;
   };
-
-in
-home-manager.lib.homeManagerConfiguration rec {
-  inherit pkgs;
   modules = [ ./home.nix ];
 }
