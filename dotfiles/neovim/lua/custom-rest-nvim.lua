ad3abc89574825acb8ed299fdca43a39d4e24895
James Santucci <james.santucci@gmail.com>
2023-08-09 16:33:28 -0600

    Move http keybindings to ftplugin

--- dotfiles/neovim/lua/custom-rest-nvim.lua
+++ dotfiles/neovim/lua/custom-rest-nvim.lua
@@ -36,14 +36,6 @@ require("rest-nvim").setup({
   yank_dry_run = true,
 })
 
--- keymappings
--- -- run the command in this file
-vim.api.nvim_set_keymap("n", "<leader>hs", "<Plug>RestNvim<CR>", { noremap = true })
--- preview the curl command
-vim.api.nvim_set_keymap("n", "<leader>hp", "<Plug>RestNvimPreview<CR>", { noremap = true })
--- rerun last command
-vim.api.nvim_set_keymap("n", "<leader>hr", "<Plug>RestNvimLast<CR>", { noremap = true })
-
 -- set files with .http extension to "http" filetype
 vim.filetype.add({
   extension = {
