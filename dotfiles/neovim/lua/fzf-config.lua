d3b4bef299634215af594151e74bb3319c35ba90
James Santucci <james.santucci@gmail.com>
2023-09-11 11:43:46 -0700

    Add binding to search home vimwiki
    Helps with #64

--- dotfiles/neovim/lua/fzf-config.lua
+++ dotfiles/neovim/lua/fzf-config.lua
@@ -13,4 +13,5 @@ vim.api.nvim_set_keymap("n", "<space>fg", "<cmd>:FzfLua git_files<CR>", { norema
 vim.api.nvim_set_keymap("n", "<space>flds", "<cmd>:FzfLua lsp_document_symbols<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<space>flws", "<cmd>:FzfLua lsp_workspace_symbols<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<space>fs", "<cmd>:FzfLua grep_project<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<space>fw", ":lua require('fzf-lua').live_grep({cwd='~/vimwiki'})<CR>", { noremap = true })
 
