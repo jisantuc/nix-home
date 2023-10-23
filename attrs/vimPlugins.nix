704476f18da498f0dcf78e042ecfa564144ce3a0
James Santucci <james.santucci@gmail.com>
2023-10-23 11:01:56 -0700

    Add and configure nvim-test to run tests from nvim

--- attrs/vimPlugins.nix
+++ attrs/vimPlugins.nix
@@ -9,6 +9,15 @@ let
       hash = "sha256-uOiKzhl+3Pi0pFLecQqUWveN+1Z3Tu/UiSPBmS+bio8=";
     };
   };
+  nvim-test = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    name = "nvim-test";
+    src = pkgs.fetchFromGitHub {
+      owner = "klen";
+      repo = "nvim-test";
+      rev = "1.4.1";
+      hash = "sha256-mMi07UbMWmC75DFfW1b+sR2uRPxizibFwS2qcN9rpLI=";
+    };
+  };
 in
 with vimPlugins;
 [
@@ -28,6 +37,7 @@ with vimPlugins;
   nvim-jqx
   nvim-lspconfig
   nvim-metals
+  nvim-test
   nvim-tree-lua
   nvim-web-devicons
   orgmode
--- dotfiles/neovim/ftplugin/python.lua
+++ dotfiles/neovim/ftplugin/python.lua
@@ -1,2 +1,28 @@
 -- add special way to run black, since pyright doesn't have a code format option
 vim.api.nvim_set_keymap("n", "<leader>f", "<cmd>!black %<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>cta", ":TestSuite<CR>", { noremap = true, desc = "Run all detected tests" })
+vim.api.nvim_set_keymap("n", "<leader>ctf", ":TestFile<CR>", { noremap = true, desc = "Run tests in this file" })
+vim.api.nvim_set_keymap("n", "<leader>ctl", ":TestLast<CR>", { noremap = true, desc = "Rerun the last run test" })
+vim.api.nvim_set_keymap("n", "<leader>ctt", ":TestNearest<CR>", { noremap = true, desc = "Run nearest test to cursor" })
+
+-- configure nvim-test
+-- python only for now, which is why it's in ftplugin instead of somewhere more general
+-- I'll move it if I end up using it often and borrowing it for other languages
+require('nvim-test').setup {
+        run = true,                     -- run tests (using for debug)
+        commands_create = true,         -- create commands (TestFile, TestLast, ...)
+        filename_modifier = ":.",       -- modify filenames before tests run(:h filename-modifiers)
+        silent = false,                 -- less notifications
+        term = "terminal",              -- a terminal to run ("terminal"|"toggleterm")
+        termOpts = {
+                direction = "vertical", -- terminal's direction ("horizontal"|"vertical"|"float")
+                width = 96,             -- terminal's width (for vertical|float)
+                height = 24,            -- terminal's height (for horizontal|float)
+                go_back = false,        -- return focus to original window after executing
+                stopinsert = "auto",    -- exit from insert mode (true|false|"auto")
+                keep_one = true,        -- keep only one terminal for testing
+        },
+        runners = {
+                python = "nvim-test.runners.pytest",
+        }
+}
