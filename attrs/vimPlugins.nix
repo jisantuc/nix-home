f4c36c2c39e9908596739416154a2312f28645cc
James Santucci <james.santucci@gmail.com>
2023-10-23 22:53:23 -0700

    🔪 Remove nvim-test

--- attrs/vimPlugins.nix
+++ attrs/vimPlugins.nix
@@ -9,15 +9,6 @@ let
       hash = "sha256-uOiKzhl+3Pi0pFLecQqUWveN+1Z3Tu/UiSPBmS+bio8=";
     };
   };
-  nvim-test = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    name = "nvim-test";
-    src = pkgs.fetchFromGitHub {
-      owner = "klen";
-      repo = "nvim-test";
-      rev = "1.4.1";
-      hash = "sha256-mMi07UbMWmC75DFfW1b+sR2uRPxizibFwS2qcN9rpLI=";
-    };
-  };
 in
 with vimPlugins;
 [
@@ -41,7 +32,6 @@ with vimPlugins;
   nvim-jqx
   nvim-lspconfig
   nvim-metals
-  nvim-test
   nvim-tree-lua
   nvim-web-devicons
   orgmode
