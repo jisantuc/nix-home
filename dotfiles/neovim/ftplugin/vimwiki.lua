384856b239206f54137e8355cc944337f9dd24ce
James Santucci <james.santucci@gmail.com>
2024-01-03 09:03:43 -0800

    Merge from main

--- dotfiles/neovim/ftplugin/vimwiki.lua
+++ dotfiles/neovim/ftplugin/vimwiki.lua
@@ -1,16 +1,16 @@
 -- list things that link to the currently opened wiki file
 vim.api.nvim_set_keymap("n", "<leader>wlb", "<cmd>VimwikiBacklinks<CR>",
-        { noremap = true, desc = "list backlinks" })
+        { noremap = true, desc = "List backlinks" })
 -- confirm all links in this wiki are reachable
 vim.api.nvim_set_keymap("n", "<leader>wlc", "<cmd>VimwikiCheckLinks<CR>",
-        { noremap = true, desc = "check links" })
+        { noremap = true, desc = "Check all links" })
 -- generate a table of contents
 vim.api.nvim_set_keymap("n", "<leader>wo", "<cmd>VimwikiTOC<CR>",
-        { noremap = true, desc = "write table of contents" })
+        { noremap = true, desc = "Insert or update table of contents" })
 
 -- preview math
 vim.api.nvim_set_keymap("n", "<leader>wmp", ":lua require('nabla').popup()<CR>",
-        { noremap = true, desc = "popup equation preview" })
+        { noremap = true, desc = "Popup equation preview" })
 -- toggle math virtual lines
 vim.api.nvim_set_keymap("n", "<leader>wmt", ":lua require('nabla').toggle_virt()<CR>",
-        { noremap = true, desc = "toggle equation virtual lines" })
+        { noremap = true, desc = "Toggle equation virtual lines" })
