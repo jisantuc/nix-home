8f91f52f3a03c7aade1e18fce6cfb267e83c7096
James Santucci <james.santucci@gmail.com>
2024-01-07 22:41:20 -0800

    add wiki filetype, move bindings, and add "insert date" binding

--- dotfiles/neovim/lua/custom-vimwiki.lua
+++ dotfiles/neovim/lua/custom-vimwiki.lua
@@ -1,7 +1,5 @@
--- list things that link to the currently opened wiki file
-vim.api.nvim_set_keymap("n", "<leader>wlb", "<cmd>VimwikiBacklinks<CR>", { noremap = true, desc = "List backlinks" })
--- confirm all links in this wiki are reachable
-vim.api.nvim_set_keymap("n", "<leader>wlc", "<cmd>VimwikiCheckLinks<CR>", { noremap = true, desc = "Check all links" })
--- generate a table of contents
-vim.api.nvim_set_keymap("n", "<leader>wo", "<cmd>VimwikiTOC<CR>",
-        { noremap = true, desc = "Insert or update table of contents" })
+vim.filetype.add({
+        extension = {
+                wiki = "wiki"
+        }
+})
