73e3093282541b1e99c1877d894a2c84c184a433
James Santucci <james.santucci@gmail.com>
2024-02-29 14:09:11 -0800

    Reconfigure rest-nvim mappings
    Something broke these -- I don't know what -- but the plugin commands are no longer
    listed with :com and nothing was happening with the old send binding

--- dotfiles/neovim/ftplugin/http.lua
+++ dotfiles/neovim/ftplugin/http.lua
@@ -1,7 +1,23 @@
 -- keymappings
+
+local restNvim = require('rest-nvim')
+
+local function selectEnv(envFile)
+        restNvim.select_env(envFile)
+end
+
+vim.api.nvim_create_user_command(
+        "HttpSelectEnv",
+        function(opts) selectEnv(opts.fargs[1] .. ".env") end,
+        { nargs = 1 }
+)
+
 -- -- run the command in this file
-vim.api.nvim_set_keymap("n", "<leader>hs", "<Plug>RestNvim<CR>", { noremap = true, desc = "Send http request" })
--- preview the curl command
-vim.api.nvim_set_keymap("n", "<leader>hp", "<Plug>RestNvimPreview<CR>", { noremap = true, desc = "Preview http request" })
--- rerun last command
-vim.api.nvim_set_keymap("n", "<leader>hr", "<Plug>RestNvimLast<CR>", { noremap = true, desc = "Rerun last http request" })
+vim.api.nvim_set_keymap("n", "<leader>hel", "<cmd>:HttpSelectEnv local<CR>",
+        { noremap = true, desc = "Use local .env file for rest-nvim" })
+vim.api.nvim_set_keymap("n", "<leader>het", "<cmd>:HttpSelectEnv test<CR>",
+        { noremap = true, desc = "Use test .env file for rest-nvim" })
+vim.api.nvim_set_keymap("n", "<leader>hep", "<cmd>:HttpSelectEnv prod<CR>",
+        { noremap = true, desc = "Use prod .env file for rest-nvim" })
+vim.api.nvim_set_keymap("n", "<leader>hs", ":lua require('rest-nvim').run()<CR>",
+        { noremap = true, desc = "Send http request" })
