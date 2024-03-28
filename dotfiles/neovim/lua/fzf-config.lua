255e21eda6cf15e5bf16e55712f2bd4eac63219e
James Santucci <james.santucci@gmail.com>
2024-03-28 14:31:07 -0600

    Add location list binding and lines / grep_last bindings (#93)
    * Add location list binding and lines / grep_last bindings
    
    * capital L for lines I guess

--- dotfiles/neovim/lua/fzf-config.lua
+++ dotfiles/neovim/lua/fzf-config.lua
@@ -1,3 +1,5 @@
+local fzfLua = require("fzf-lua")
+
 -- make find much better
 vim.opt.path:remove "/usr/include"
 vim.opt.path:append "**"
@@ -8,18 +10,25 @@ vim.opt.wildignore:append "**/target/*"
 vim.opt.wildignore:append "**/.bsp/*"
 vim.opt.wildignore:append "**/.metals/*"
 vim.api.nvim_set_keymap("n", "<space>fb", "<cmd>:FzfLua buffers<CR>", { noremap = true, desc = "Choose open buffer" })
+vim.api.nvim_set_keymap("n", "<space>fL", "<cmd>:FzfLua lines<CR>", { noremap = true, desc = "Choose file in workspace" })
 vim.api.nvim_set_keymap("n", "<space>ff", "<cmd>:FzfLua files<CR>", { noremap = true, desc = "Choose file in workspace" })
 vim.api.nvim_set_keymap("n", "<space>fg", "<cmd>:FzfLua git_files<CR>", { noremap = true, desc = "Choose git file" })
-vim.api.nvim_set_keymap("n", "<space>flds", "<cmd>:FzfLua lsp_document_symbols<CR>",
-        { noremap = true, desc = "Search document symbols" })
-vim.api.nvim_set_keymap("n", "<space>flws", "<cmd>:FzfLua lsp_workspace_symbols<CR>",
-        { noremap = true, desc = "Search workspace symbols" })
 vim.api.nvim_set_keymap("n", "<space>fs", "<cmd>:FzfLua grep_project<CR>",
         { noremap = true, desc = "Search string in project" })
+vim.api.nvim_set_keymap("n", "<space>fls", "<cmd>:FzfLua grep_last<CR>",
+        { noremap = true, desc = "Repeat last search" })
 vim.api.nvim_set_keymap("n", "<space>fws", ":lua require('fzf-lua').live_grep({cwd='~/vimwiki'})<CR>",
         { noremap = true, desc = "Search in vimwiki" })
 vim.api.nvim_set_keymap("n", "<space>fwf", ":lua require('fzf-lua').files({cwd='~/vimwiki'})<CR>",
         { noremap = true, desc = "Search files in vimwiki" })
 
 -- make fzf the default select ui
-require("fzf-lua").register_ui_select()
+fzfLua.register_ui_select()
+
+fzfLua.setup({
+        keymap = {
+                fzf = {
+                        ["ctrl-a"] = "select-all+accept"
+                }
+        }
+})
