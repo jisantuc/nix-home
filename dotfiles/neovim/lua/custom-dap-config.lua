3a8ca6adef306cdb567ea375c03b178f837e16c9
James Santucci <james.santucci@gmail.com>
2025-01-23 10:32:48 -0800

    Shorten debug UI toggle
    There's nothing else on <leader>cdu, so save a keystroke on something
    you do like a million times every day.

--- dotfiles/neovim/lua/custom-dap-config.lua
+++ dotfiles/neovim/lua/custom-dap-config.lua
@@ -3,6 +3,6 @@ require("nvim-dap-virtual-text").setup()
 require("dapui").setup()
 
 vim.api.nvim_set_keymap("n",
-        "<leader>cdut",
+        "<leader>cdu",
         "<cmd>lua require('dapui').toggle()<CR>",
         { noremap = true, desc = "Toggle debug adapter UI" })
