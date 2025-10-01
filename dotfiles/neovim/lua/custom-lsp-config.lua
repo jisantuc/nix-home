4ca6b6b937eb7dacbdff36e5132cdf43a4bbf051
James Santucci <james.santucci@gmail.com>
2025-09-30 18:44:40 -0700

    Add c lsp (#121)

--- dotfiles/neovim/lua/custom-lsp-config.lua
+++ dotfiles/neovim/lua/custom-lsp-config.lua
@@ -2,16 +2,16 @@ vim.api.nvim_set_keymap("n", "<leader>cl", "<cmd>lua vim.lsp.codelens.run()<CR>"
 vim.api.nvim_set_keymap("n", "<leader>d", "<cmd>lua vim.diagnostic.setloclist()<CR>",
         { noremap = true, desc = "Show buffer diagnostics" })
 
-vim.lsp.enable("elixir")
-vim.lsp.enable("python")
-vim.lsp.enable("lua")
-vim.lsp.enable("nix")
-vim.lsp.enable("typst")
+local enabled_lsp_langs = { "elixir", "python", "lua", "nix", "typst", "c" }
+
+for _, lang in ipairs(enabled_lsp_langs) do
+        vim.lsp.enable(lang)
+end
 
 vim.api.nvim_create_autocmd(
         { "BufEnter", "CursorHold", "InsertLeave" }, {
                 callback = function(event)
                         vim.lsp.codelens.refresh({ bufnr = 0 })
                 end,
-                pattern = { "*.nix", "*.py", "*.lua", "*.ex", "*.typ", "*.typst" }
+                pattern = { "*.nix", "*.py", "*.lua", "*.ex", "*.typ", "*.typst", "*.c" }
         })
