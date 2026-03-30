6f4ae14db748d1b678e96a6229618cb99c627ea7
James Santucci <james.santucci@gmail.com>
2026-03-30 14:05:12 -0700

    Bind CreateCards command in vimwiki files

--- dotfiles/neovim/ftplugin/vimwiki.lua
+++ dotfiles/neovim/ftplugin/vimwiki.lua
@@ -4,7 +4,17 @@ vim.api.nvim_set_keymap("n", "<leader>wlb", "<cmd>VimwikiBacklinks<CR>", { norem
 vim.api.nvim_set_keymap("n", "<leader>wlc", "<cmd>VimwikiCheckLinks<CR>", { noremap = true, desc = "Check all links" })
 -- generate a table of contents
 vim.api.nvim_set_keymap("n", "<leader>wo", "<cmd>VimwikiTOC<CR>",
-        { noremap = true, desc = "Insert or update table of contents" })
+  { noremap = true, desc = "Insert or update table of contents" })
 
 vim.opt_local.spell = true
 vim.opt_local.shiftwidth = 2
+
+local function createCards()
+  local resultTable = vim.system({ "repeater", "create", vim.fn.expand("%") },
+    { text = true, cwd = vim.fn.expand("$HOME") .. "/srs" }):wait()
+  local stdErr = resultTable["stderr"]
+  local expectedErrorStart = string.find(stdErr, "Device not configured")
+  if expectedErrorStart < 0 then print(stdErr) else print("Cards created 📝") end
+end
+
+vim.api.nvim_create_user_command("CreateCards", createCards, {})
