8401450294639679deb742290cc099c7bde5c706
James Santucci <james.santucci@gmail.com>
2026-04-21 09:32:20 -0700

    Remove env selector hotkeys
    Just use :Rest env select instead

--- dotfiles/neovim/ftplugin/http.lua
+++ dotfiles/neovim/ftplugin/http.lua
@@ -1,10 +1,3 @@
--- run the command in this file
-vim.api.nvim_set_keymap("n", "<leader>hel", "<cmd>:Rest env set local.env<CR>",
-        { noremap = true, desc = "Use local .env file for rest-nvim" })
-vim.api.nvim_set_keymap("n", "<leader>het", "<cmd>:Rest env set test.env<CR>",
-        { noremap = true, desc = "Use test .env file for rest-nvim" })
-vim.api.nvim_set_keymap("n", "<leader>hep", "<cmd>:Rest env set prod.env<CR>",
-        { noremap = true, desc = "Use prod .env file for rest-nvim" })
 vim.api.nvim_set_keymap("n", "<leader>hs", "<cmd>:Rest run<CR>",
         { noremap = true, desc = "Send http request" })
 vim.api.nvim_set_keymap("n", "<leader>dd", "<cmd>DataDogTraceLogs<CR>",
