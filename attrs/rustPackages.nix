69b3d717c91a7373884b0ccd9a544263c9664304
James Santucci <james.santucci@gmail.com>
2026-06-06 15:59:30 -0700

    Update repeater

--- attrs/rustPackages.nix
+++ attrs/rustPackages.nix
@@ -6,16 +6,16 @@
   repeater = rustPlatform.buildRustPackage
     (finalAttrs: {
       pname = "repeater";
-      version = "v0.1.9";
+      version = "v0.1.10";
 
       src = fetchFromGitHub {
         owner = "shaankhosla";
         repo = "repeater";
         tag = finalAttrs.version;
-        hash = "sha256-vkiDJ9hGEa9PCaNjgGJAvAFGUxXJK04ezvXyAmJB3bk=";
+        hash = "sha256-8TWqY78w5cOirLnrBONiratAZQSQrYpp++5dXRlFlNo=";
       };
 
-      cargoHash = "sha256-/ttQpk9JnSRhE/VOJlz60LpV1PJ/spzXQ1EPLcox1mw=";
+      cargoHash = "sha256-hngZ55o1YsnstBGjp8++9SsxwfUyu+X4YwZuzMupFTE=";
 
     });
 }
