8f052994027694ddc24deeee9760345b277949bb
James Santucci <james.santucci@gmail.com>
2026-08-20 23:10:37 -0700

    Bring back elm lsp

--- dotfiles/neovim/lua/custom-lsp-config.lua
+++ dotfiles/neovim/lua/custom-lsp-config.lua
@@ -2,7 +2,7 @@ vim.api.nvim_set_keymap("n", "<leader>cl", "<cmd>lua vim.lsp.codelens.run()<CR>"
 vim.api.nvim_set_keymap("n", "<leader>d", "<cmd>lua vim.diagnostic.setloclist()<CR>",
         { noremap = true, desc = "Show buffer diagnostics" })
 
-local enabled_lsp_langs = { "elixir", "python", "lua", "nix", "typst", "c", "jinja" }
+local enabled_lsp_langs = { "elixir", "python", "lua", "nix", "typst", "c", "jinja", "elm" }
 
 for _, lang in ipairs(enabled_lsp_langs) do
         vim.lsp.enable(lang)
@@ -13,5 +13,5 @@ vim.api.nvim_create_autocmd(
                 callback = function(event)
                         vim.lsp.codelens.refresh({ bufnr = 0 })
                 end,
-                pattern = { "*.nix", "*.py", "*.lua", "*.ex", "*.typ", "*.typst", "*.c", ".j2" }
+                pattern = { "*.nix", "*.py", "*.lua", "*.ex", "*.typ", "*.typst", "*.c", ".j2", ".elm" }
         })
